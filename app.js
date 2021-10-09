ece37b0b3b79c31a9c1731925c08c83ecc18ffd4
alvinbithealth <alvin.ashari@bithealth.co.id>
2021-10-09 15:20:49 +0700

    Initial Project

---
+++ app.js
@@ -0,0 +1,37 @@
+const amqp = require('amqplib');
+
+const amqpConfig = 'amqp://localhost';
+const queueName = 'send-message';
+
+const getBlogPost = (id) => ([
+    {
+        id: 1,
+        title: "Membuat Sesuatu Menggunakan Node.js",
+        thumb: "https://via.placeholder.com/150",
+    },
+    {
+        id: 2,
+        title: "Membuat Sesuatu Menggunakan Node.js",
+        thumb: "https://via.placeholder.com/150",
+    }
+].find((value => value.id === id)));
+
+(async () => {
+    const connection = await amqp.connect(amqpConfig);
+    const channel = await connection.createChannel();
+
+    channel.assertQueue(queueName, { durable: false });
+    channel.prefetch(1);
+    console.log(' [x] Awaiting RPC requests');
+    channel.consume(queueName, (msg) => {
+        const id = parseInt(msg.content.toString());
+        console.log(` [.] getBlogPost(${id})`);
+        const result = getBlogPost(id);
+        // console.log(result)
+
+        channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(result)),
+            { correlationId: msg.properties.correlationId }
+        );
+        channel.ack(msg);
+    });
+})();
